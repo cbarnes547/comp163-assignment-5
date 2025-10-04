c0c869971a981f3b61557bef36167fd8d4c2f157
cbarnes547 <chloeebarnes547@gmail.com>
2025-10-03 20:59:10 -0400

    Challenge 3:  Multiplication grid - nested loops for 2D data

--- cebarnes3_assignment_5.py
+++ cebarnes3_assignment_5.py
@@ -37,3 +37,18 @@ for i in range(2, num):
 if prime:
     print(f"{num} is prime!")
 print()
+
+#Test case 3
+print("=== Challenge 3: Multiplication Table ===")
+print("Multiplication Table:")
+
+print("   ", end="")
+for j in range(1, 11):
+    print(f"{j:4}", end="")
+print()
+
+for i in range(1, 11):
+    print(f"{i:2} ", end="")
+    for j in range(1, 11):
+        print(f"{i*j:4}", end="")
+    print()
